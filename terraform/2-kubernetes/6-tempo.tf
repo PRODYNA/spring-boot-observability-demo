30a8dddff8133db18b2c504dc26a7d571027f931
Darko Križić <darko@krizic.net>
2024-12-17 18:47:48 +0100

    Using distributed version of tempo

--- terraform/2-kubernetes/6-tempo.tf
+++ terraform/2-kubernetes/6-tempo.tf
@@ -5,14 +5,14 @@ resource "kubernetes_namespace" "tempo" {
 }
 
 resource "helm_release" "tempo" {
-  chart      = "tempo"
+  chart      = "tempo-distributed"
   repository = local.helm.repository.grafana
   name       = "tempo"
   namespace  = kubernetes_namespace.tempo.metadata[0].name
   version    = "1.16.0"
 
   values = [
-    file("helm/tempo.yaml"),
+    file("helm/tempo-distributed.yaml"),
   ]
 
   depends_on = [
