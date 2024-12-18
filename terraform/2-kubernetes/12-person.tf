a6ae060dc777d30768853f49c22c89be1de3bc49
Darko Križić <darko@krizic.net>
2024-12-18 14:36:59 +0100

    App now reachable

--- terraform/2-kubernetes/12-person.tf
+++ terraform/2-kubernetes/12-person.tf
@@ -5,12 +5,22 @@ resource "kubernetes_namespace" "person" {
 }
 
 resource "helm_release" "person" {
-  chart      = "../../chart/person"
-  name       = "person"
-  namespace  = kubernetes_namespace.person.metadata[0].name
-  version    = "0.1.0"
+  chart     = "../../chart/person"
+  name      = "person"
+  namespace = kubernetes_namespace.person.metadata[0].name
+  version   = "0.1.0"
 
   values = [
-      file("helm/person.yaml"),
+    file("helm/person.yaml"),
   ]
+
+  set {
+    name  = "ingress.hosts[0].host"
+    value = data.terraform_remote_state.azure.outputs.app_name
+  }
+
+  set {
+    name  = "ingress.tls[0].hosts[0]"
+    value = data.terraform_remote_state.azure.outputs.app_name
+  }
 }
