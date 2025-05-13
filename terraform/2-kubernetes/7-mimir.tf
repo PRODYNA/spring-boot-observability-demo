f581016e7d07e337a3868fcb125f5d9e64ded3bc
Darko Krizic <darko.krizic@prodyna.com>
2025-05-13 12:35:06 +0200

    Fix mimir release

--- terraform/2-kubernetes/7-mimir.tf
+++ terraform/2-kubernetes/7-mimir.tf
@@ -8,14 +8,14 @@ resource "helm_release" "mimir" {
   chart      = "mimir-distributed"
   repository = local.helm.repository.grafana
   name       = "mimir"
-  namespace  = kubernetes_namespace.mimir.metadata[0].name
+  namespace  = data.kubernetes_namespace.mimir.metadata[0].name
   version    = "5.6.0"
 
   values = [
     file("helm/mimir.yaml"),
   ]
 
-  depends_on = [
-    helm_release.prometheus-operator-crds
-  ]
+  # depends_on = [
+  #   helm_release.prometheus-operator-crds
+  # ]
 }
