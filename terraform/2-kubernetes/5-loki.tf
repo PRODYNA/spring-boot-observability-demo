11d2adee98bd183210abc639d0ad3b7fa329d479
Darko Krizic <darko.krizic@prodyna.com>
2025-05-13 11:55:09 +0200

    Try to install Loki

--- terraform/2-kubernetes/5-loki.tf
+++ terraform/2-kubernetes/5-loki.tf
@@ -4,18 +4,18 @@ data "kubernetes_namespace" "loki" {
   }
 }
 
-# resource "helm_release" "loki" {
-#     chart      = "loki"
-#     repository = local.helm.repository.grafana
-#     name       = "loki"
-#     namespace  = data.kubernetes_namespace.loki.metadata[0].name
-#     version    = "6.27.0"
-#
-#     values = [
-#         file("helm/loki.yaml"),
-#     ]
-#
-#     depends_on = [
-#         helm_release.prometheus-operator-crds
-#     ]
-# }
+resource "helm_release" "loki" {
+    chart      = "loki"
+    repository = local.helm.repository.grafana
+    name       = "loki"
+    namespace  = data.kubernetes_namespace.loki.metadata[0].name
+    version    = "6.27.0"
+
+    values = [
+        file("helm/loki.yaml"),
+    ]
+
+#    depends_on = [
+#        helm_release.prometheus-operator-crds
+#    ]
+}
