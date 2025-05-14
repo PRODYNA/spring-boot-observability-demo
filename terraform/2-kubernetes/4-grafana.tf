3d7f121c4bbe80cf71f3f52ff64584a801feea39
Darko Krizic <darko.krizic@prodyna.com>
2025-05-14 15:48:01 +0200

    Try loki again

--- terraform/2-kubernetes/4-grafana.tf
+++ terraform/2-kubernetes/4-grafana.tf
@@ -1,38 +1,28 @@
-# resource "helm_release" "grafana" {
-#   chart      = "grafana"
-#   repository = local.helm.repository.grafana
-#   name       = "grafana"
-#   namespace  = kubernetes_namespace.observability.metadata[0].name
-#   version    = "8.10.1"
-#
-#   values = [
-#     file("helm/grafana.yaml"),
-#   ]
-#
-#   set {
-#     name  = "ingress.hosts[0]"
-#     value = data.terraform_remote_state.azure.outputs.grafana_hostname
-#   }
-#
-#   set {
-#     name  = "ingress.tls[0].hosts[0]"
-#     value = data.terraform_remote_state.azure.outputs.grafana_hostname
-#   }
-#
-#   depends_on = [
-#     helm_release.prometheus-operator-crds
-#   ]
-# }
-#
-# resource "kubernetes_config_map_v1" "grafana-dashboard-worldmap" {
-#   metadata {
-#     name      = "grafana-dashboard-worldmap"
-#     namespace = kubernetes_namespace.observability.metadata[0].name
-#     labels = {
-#       "grafana_dashboard" = "1"
-#     }
-#   }
-#   data = {
-#       "worldmap.json" = file("dashboard/worldmap.yaml")
-#   }
-# }
+resource "helm_release" "grafana" {
+  chart      = "grafana"
+  repository = local.helm.repository.grafana
+  name       = "grafana"
+  namespace  = data.kubernetes_namespace.observability.metadata[0].name
+  version    = "8.10.1"
+
+  values = [
+    file("helm/grafana.yaml"),
+  ]
+
+  #depends_on = [
+  #  helm_release.prometheus-operator-crds
+  #]
+}
+
+resource "kubernetes_config_map_v1" "grafana-dashboard-worldmap" {
+  metadata {
+    name      = "grafana-dashboard-worldmap"
+    namespace = data.kubernetes_namespace.observability.metadata[0].name
+    labels = {
+      "grafana_dashboard" = "1"
+    }
+  }
+  data = {
+      "worldmap.json" = file("dashboard/worldmap.yaml")
+  }
+}
--- terraform/2-kubernetes/7-mimir.tf
+++ terraform/2-kubernetes/7-mimir.tf
@@ -4,18 +4,18 @@ data "kubernetes_namespace" "mimir" {
   }
 }
 
-resource "helm_release" "mimir" {
-  chart      = "mimir-distributed"
-  repository = local.helm.repository.grafana
-  name       = "mimir"
-  namespace  = data.kubernetes_namespace.mimir.metadata[0].name
-  version    = "5.6.0"
-
-  values = [
-    file("helm/mimir.yaml"),
-  ]
-
-  # depends_on = [
-  #   helm_release.prometheus-operator-crds
-  # ]
-}
+# resource "helm_release" "mimir" {
+#   chart      = "mimir-distributed"
+#   repository = local.helm.repository.grafana
+#   name       = "mimir"
+#   namespace  = data.kubernetes_namespace.mimir.metadata[0].name
+#   version    = "5.6.0"
+#
+#   values = [
+#     file("helm/mimir.yaml"),
+#   ]
+#
+#   # depends_on = [
+#   #   helm_release.prometheus-operator-crds
+#   # ]
+# }
